9fe3a31c441a1ac14bb1a9fe89415144b70dbc7b
MadeInShineA <olivier.amacker@netplus.ch>
2023-05-24 01:13:24 +0200

    Added the nav buttons inside the DefaultLayout.vue

--- resources/js/layouts/DefaultLayout.vue
+++ resources/js/layouts/DefaultLayout.vue
@@ -1,11 +1,27 @@
 <template>
     <h1>Test</h1>
-        <router-view></router-view>
+    <router-view></router-view>
+    <div>
+        <router-link to="/home">
+            Go to Home
+        </router-link>
+    </div>
+
+    <div>
+        <router-link to="/map">
+            Go to Map
+        </router-link>
+    </div>
+
 </template>
 
 <script>
 export default {
     name: "DefaultLayout",
+
+    mounted() {
+        console.log('DefaultLayout Mounted')
+    }
 }
 </script>
 
