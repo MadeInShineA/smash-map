b1ae54219bb3ffbb5341e326aa559440a4d6dc5d
MadeInShineA <olivier.amacker@netplus.ch>
2023-05-31 20:37:25 +0200

    Deleted the earth-rotating.gif and added a TODO to filesystems.php

--- config/filesystems.php
+++ config/filesystems.php
@@ -12,7 +12,7 @@ return [
     | based disks are available to your application. Just store away!
     |
     */
-
+    #TODO Change local to public ?
     'default' => env('FILESYSTEM_DISK', 'local'),
 
     /*
