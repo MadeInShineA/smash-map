af3c2c719584425c95de831053f278154fed5c5c
MadeInShineA <olivier.amacker@netplus.ch>
2023-07-06 20:54:54 +0200

    Changed the game's colors

--- database/seeders/GameSeeder.php
+++ database/seeders/GameSeeder.php
@@ -15,11 +15,11 @@ class GameSeeder extends Seeder
         $games = [
             ['id' => 4,     'name' => '64',         'color' => '#FAC41A'],
             ['id' => 1,     'name' => 'Melee',      'color' => '#A30010'],
-            ['id' => 5,     'name' => 'Brawl',      'color' => '#2E130F'],
+            ['id' => 5,     'name' => 'Brawl',      'color' => '#660d02'],
             ['id' => 2,     'name' => 'Project M',  'color' => '#3B448B'],
             ['id' => 33602, 'name' => 'Project +',  'color' => '#6FD19C'],
             ['id' => 3,     'name' => '3DS/WiiU',   'color' => '#AFC1EE'],
-            ['id' =>1386,   'name' => 'Ultimate',   'color' => '#000000']
+            ['id' =>1386,   'name' => 'Ultimate',   'color' => '#F18A41']
         ];
 
         DB::table('games')->insert($games);
