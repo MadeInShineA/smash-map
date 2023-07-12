ae5c7c2a0ad4f25cfe627d4567e6fb1356e471f0
MadeInShineA <olivier.amacker@netplus.ch>
2023-07-12 22:12:31 +0200

    Added the InternalDomain middleware to all the api's route based on the SMASH_MAP_URL inside .env

---
+++ app/Http/Middleware/InternalDomain.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class InternalDomain
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+
+        $requestHost = $request->getHost();
+
+        if ($requestHost == env('SMASH_MAP_URL')) {
+            return $next($request);
+        }
+        return response()->json(['message' => 'Forbidden - 403E - ' . $requestHost], 403);
+        }
+}
