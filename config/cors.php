febd05664b08d1e69128c8b84e066a12137cb331
MadeInShineA <olivier.amacker@netplus.ch>
2023-07-24 22:03:44 +0200

    Removed cors

--- config/cors.php
+++ config/cors.php
@@ -15,15 +15,15 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => [],
 
-    'allowed_methods' => ['*'],
+    'allowed_methods' => [],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => [],
 
     'allowed_origins_patterns' => [],
 
-    'allowed_headers' => ['*'],
+    'allowed_headers' => [],
 
     'exposed_headers' => [],
 
