00083947dd6a06ba8ee2b22b71cc3cdd908dab7c
MadeInShineA <olivier.amacker@netplus.ch>
2024-04-30 10:51:31 +0200

    Added the address to the LocalStorageUser.php to add it to local storage when registering / connecting

--- app/Http/Resources/User/LocalStorageUser.php
+++ app/Http/Resources/User/LocalStorageUser.php
@@ -12,9 +12,13 @@ class LocalStorageUser extends JsonResource
         return [
             'id'                => $this->id,
             'profilePicture'    => $this->profile_picture,
-            'settings'        => [
-                'hasDistanceNotifications' => $this->has_distance_notifications,
-                'distanceNotificationsRadius' => $this->distance_notifications_radius
+            'settings'          => [
+                'hasDistanceNotifications'      => $this->has_distance_notifications,
+                'distanceNotificationsRadius'   => $this->distance_notifications_radius,
+                'address'                       => [
+                    'lat'      => $this->address->latitude,
+                    'lng'     => $this->address->longitude,
+                ],
             ],
         ];
     }
