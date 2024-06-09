ef3ca12e3f75cbaf856591fdf8d999f97d5f5360
MadeInShineA <olivier.amacker@netplus.ch>
2024-06-09 16:45:20 +0200

    Added viewport meta to app.blade.php

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,6 +4,7 @@
     <title>Smash Map</title>
     <link rel="icon" type="image/x-icon" href="{{@asset('images/logo-no-text-white-globe.png')}}">
     <link id="theme-link" rel="stylesheet" href="{{@asset('themes/mdc-dark-indigo/theme.css')}}">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     @vite('resources/css/app.css')
 </head>
 <body>
