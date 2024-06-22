0e851545e105b38f43a20a04639bd5ab137798a7
MadeInShineA <olivier.amacker@netplus.ch>
2024-06-22 19:00:16 +0200

    Began adding push notifications with Pusher Beams

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -11,6 +11,7 @@
     <meta name="theme-color" content="#ffffff"/>
     <link rel="apple-touch-icon" href="{{ asset('/images/logo-512.png') }}">
     <link rel="manifest" href="{{ asset('/manifest.json') }}">
+
 </head>
 <body>
     <div id="app"></div>
@@ -33,6 +34,24 @@
             console.error("Service workers are not supported.");
         }
     </script>
+
+    <!-- Pusher Beam (Push notifications)  -->
+    <script src="https://js.pusher.com/beams/1.0/push-notifications-cdn.js"></script>
+
+
+
+    <script>
+        const beamsClient = new PusherPushNotifications.Client({
+            instanceId: '{{ env('PUSHER_BEAMS_INSTANCE_ID') }}',
+        });
+
+        beamsClient.start()
+            .then(() => beamsClient.addDeviceInterest('hello'))
+            .then(() => console.log('Successfully registered and subscribed!'))
+            .catch(console.error);
+    </script>
+
+
 </body>
 </html>
 
