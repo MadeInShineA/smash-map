8e51de27e06a124fccbcb8dd326d9138581d3f92
MadeInShineA <olivier.amacker@netplus.ch>
2024-06-23 19:31:39 +0200

    Removed Pusher Beam for inside app.blade.php + updated manifest.json

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -33,21 +33,6 @@
         }
     </script>
 
-    <!-- Pusher Beam (Push notifications)  -->
-    <script src="https://js.pusher.com/beams/1.0/push-notifications-cdn.js"></script>
-
-
-    <script>
-        const beamsClient = new PusherPushNotifications.Client({
-            instanceId: '{{ env('PUSHER_BEAMS_INSTANCE_ID') }}',
-        });
-
-        // beamsClient.start()
-        //     .then(() => beamsClient.addDeviceInterest('hello'))
-        //     .then(() => console.log('Successfully registered and subscribed!'))
-        //     .catch(console.error);
-    </script>
-
 
 </body>
 </html>
