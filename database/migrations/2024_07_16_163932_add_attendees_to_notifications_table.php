3c75f76bd7d458d7ed4b14398302a5ed5f9e135a
MadeInShineA <olivier.amacker@netplus.ch>
2024-07-16 19:04:46 +0200

    Fixed typo inside add_attendees_to_notifications_table migration

--- database/migrations/2024_07_16_163932_add_attendees_to_notifications_table.php
+++ database/migrations/2024_07_16_163932_add_attendees_to_notifications_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('notification', function (Blueprint $table) {
+        Schema::table('notifications', function (Blueprint $table) {
             $table->integer('attendees')->nullable()->after('type');
         });
     }
