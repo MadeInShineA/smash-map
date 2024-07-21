203937e740ab2fb7e1ea685ee38cb3181ad2bee8
MadeInShineA <olivier.amacker@netplus.ch>
2024-07-21 18:55:27 +0200

    getProfilePictureAttribute hotfix

--- app/Models/User.php
+++ app/Models/User.php
@@ -122,7 +122,6 @@ class User extends Authenticatable
 
         if(!$image){
             $image = new Image();
-            $image->url = 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/1024px-No_image_available.svg.png';
         }
 
         return $image;
