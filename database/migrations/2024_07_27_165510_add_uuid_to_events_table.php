53ce8bceabcdbc8f6fd5a454909389f20f4f5c8a
MadeInShineA <olivier.amacker@netplus.ch>
2024-07-27 19:08:46 +0200

    Fixed the uuid migration

--- database/migrations/2024_07_27_165510_add_uuid_to_events_table.php
+++ database/migrations/2024_07_27_165510_add_uuid_to_events_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Scopes\ShownScope;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -15,7 +16,7 @@ return new class extends Migration
             $table->uuid('uuid')->after('id')->nullable();
         });
 
-        $events = \App\Models\Event::all();
+        $events = \App\Models\Event::withoutGlobalScope(ShownScope::class)->all();
         foreach ($events as $event) {
             $event->uuid = \Illuminate\Support\Str::uuid();
             $event->save();
