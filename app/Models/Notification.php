046ab4b6875fcef07fcf7047dc01df289672c5cd
MadeInShineA <olivier.amacker@netplus.ch>
2024-07-27 22:56:22 +0200

    Added the image_url_label to NotificationResource.php via Notification.php

--- app/Models/Notification.php
+++ app/Models/Notification.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Support\Facades\URL;
 
 class Notification extends Model
 {
@@ -36,4 +37,17 @@ class Notification extends Model
     {
         return $this->belongsTo(Event::class);
     }
+
+    public function getImageUrlLabelAttribute(): string
+    {
+        $url = $this->image_url;
+        $url = strstr($url, '/storage');
+
+        if(file_exists(public_path($url))){
+            return $this->image_url;
+        }
+
+        return URL::to('/storage/map-icons/' . $this->game->slug . '.png');
+
+        }
 }
